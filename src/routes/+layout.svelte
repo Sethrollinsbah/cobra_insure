12b063048b7fa9c68ef22e298636f88c1caf00cc
Seth Rollins <sethrollins@Seths-MacBook-Air.local>
2024-06-28 20:12:14 -0400

    final revisions at tea and poet

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,6 +16,46 @@
   ];
 </script>
 
+<svelte:head>
+  <title
+    >Alexander Health Consulting | Health insurance agent ceritified throughout
+    South Florida</title
+  >
+
+  <meta
+    property="og:title"
+    content="Alexander Health Consulting | Health insurance agent ceritified throughout South Florida."
+  />
+  <meta
+    property="twitter:title"
+    content="Alexander Health Consulting | Health insurance agent ceritified throughout South Florida."
+  />
+  <meta
+    name="description"
+    content="Alexander is a healthcare insurance agent dedicated to getting his clients the maximum value for their dollar."
+  />
+  <meta
+    name="description"
+    content="Alexander is a healthcare insurance agent dedicated to getting his clients the maximum value for their dollar."
+  />
+  <meta
+    property="og:description"
+    content="Alexander is a healthcare insurance agent dedicated to getting his clients
+  the maximum value for their dollar."
+  />
+  <meta
+    property="twitter:description"
+    content="Alexander is a healthcare insurance agent dedicated to getting his clients the maximum value for their dollar."
+  />
+  <meta property="og:image" content="/businesscard.jpeg" />
+  <meta property="twitter:image" content="/businesscard.jpeg" />
+  <meta property="og:image:alt" content="Family Health Insurance" />
+  <meta property="twitter:image:alt" content="Family Health Insurance" />
+  <meta property="og:url" content="AlexanderHealthConsulting.com" />
+  <meta property="twitter:url" content="AlexanderHealthConsulting.com" />
+  <meta property="twitter:card" content="/businesscard.jpeg" />
+</svelte:head>
+
 <section class="flex h-screen w-screen overflow-y-scroll bg-gray-100">
   <section class="relative flex h-full w-screen">
     <section class="w-full">
