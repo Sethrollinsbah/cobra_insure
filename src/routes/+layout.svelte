ab11818faa5d5a6ac75a50c7682619b363cbcb67
Seth Rollins <sethrollins@Seths-MacBook-Air.local>
2024-10-29 12:05:06 -0400

    change port

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -41,7 +41,7 @@
     property="twitter:description"
     content="Alexander is a healthcare insurance agent dedicated to getting his clients the maximum value for their dollar."
   />
-  <meta property="og:image" content="/icon.svg" />
+  <meta property="og:image" content="/logo.svg" />
   <meta property="twitter:image" content="/businesscard.jpeg" />
   <meta property="twitter:image:alt" content="Family Health Insurance" />
   <meta property="og:url" content="cobrainsure.org" />
